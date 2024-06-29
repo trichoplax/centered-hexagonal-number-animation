0602fba96586c0918b4f06d9fea32ebbb9f68fff
trichoplax <xalpohcirt@gmail.com>
2024-06-29 23:51:24 +0100

    Complete the animation

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,29 @@
-// Coloured hexagon (masking a filled circle?)
-// Animated filling of a hexagon
-// Animated colour change of a filled hexagon
-// Progressive filling and colour change of 1 hexagon, then its 7 neighbours
-// Progressive filling and colour change of each successive layer
-// Fade back to white fill at the end
 use std::f64::consts::TAU;
 use svg::node::element::path::Data;
 use svg::node::element::Animate;
 use svg::node::element::Circle;
+use svg::node::element::ClipPath;
 use svg::node::element::Definitions;
 use svg::node::element::Path as SVGPath;
 use svg::node::element::Rectangle;
 use svg::node::element::Use;
 use svg::Document;
-const ANIMATION_FRAMES_DIRECTORY_NAME: &str = "animation_frames";
+const ANIMATION_DIRECTORY_NAME: &str = "animation";
 const ROOT_3: f64 = 1.7320508075688772;
 
 fn main() {
     let grid_size: u32 = 4; // Size 0 is a single hexagon, size N+1 is size N plus a ring of hexagons
-                            //    let number_of_hexagons = (grid_size + 1).pow(3) - grid_size.pow(3);
     let hexagon_radius = 10.0;
     let hexagon_height = hexagon_radius * ROOT_3;
     let stroke_width = 1.0;
     let hexagon_rounded_corner_radius = stroke_width * 1.5;
-    let image_width = hexagon_radius * (5.0 + 3.0 * grid_size as f64) + stroke_width * 2.0 / ROOT_3;
-    let image_height = hexagon_height * (3.0 + 2.0 * grid_size as f64) + stroke_width;
+    let image_width = hexagon_radius * (4.0 * grid_size as f64 - 2.0) + stroke_width * 2.0;
+    let image_height = hexagon_height * 2.0 * (grid_size as f64 + 0.5) + stroke_width * 2.0;
     let centre = Coordinates {
         x: image_width / 2.0,
         y: image_height / 2.0,
     };
     let hexagon_angle = TAU / 6.0;
-    let hexagon_angle_in_degrees = hexagon_angle * 360.0 / TAU;
     let grid_cell_location_rotation = TAU / 12.0;
 
     let hexagon_straight_line_endpoints: Vec<(f64, f64)> = (0..=5)
@@ -134,29 +127,70 @@ fn main() {
         .set("fill", "none")
         .set("stroke", "black")
         .set("stroke-width", stroke_width)
-        .set("d", hexagon_data)
+        .set("d", hexagon_data.clone())
         .set("id", "hexagon");
 
-    let circle_definition = Circle::new()
-        .set("cx", 0)
-        .set("cy", 0)
-        .set("r", 30)
-        .set("fill", "red")
-        .set("id", "circle");
+    let filled_hexagon_definition = SVGPath::new()
+        .set("fill", "black")
+        .set("stroke", "none")
+        .set("stroke-width", stroke_width)
+        .set("d", hexagon_data)
+        .set("id", "filled-hexagon");
 
-    let animated_definition = Animate::new()
-        .set("attributeName", "r")
-        .set("from", 0)
-        .set("to", image_width)
-        .set("dur", "5s")
-        .set("repeatCount", "indefinite")
-        .set("id", "animated");
+    let hexagon_clip_definition = ClipPath::new()
+        .set("id", "hexagon-clip")
+        .add(filled_hexagon_definition);
 
-    let animated_circle_definition = circle_definition.add(animated_definition);
-
-    let definitions = Definitions::new()
+    let mut definitions = Definitions::new()
         .add(hexagon_definition)
-        .add(animated_circle_definition);
+        .add(hexagon_clip_definition);
+
+    let duration = (grid_size as f64 + 1.0) * 2.0 + 2.0;
+
+    for level in 0..=grid_size {
+        let circle_definition = Circle::new()
+            .set("cx", centre.x)
+            .set("cy", centre.y)
+            .set("id", format!("level{}-circle", level))
+            .set("clip-path", "url(#hexagon-clip)");
+
+        let start_growing = (level as f64 * 2.0) / duration;
+        let stop_growing = start_growing + 1.0 / duration;
+        let stop_colour_change = stop_growing + 1.0 / duration;
+        let start_fading = (duration - 1.0) / duration;
+
+        let animated_radius_definition = Animate::new()
+            .set("attributeName", "r")
+            .set("dur", format!("{}s", duration))
+            .set("repeatCount", "indefinite")
+            .set(
+                "values",
+                format!("0;0;{};{}", hexagon_radius, hexagon_radius),
+            )
+            .set(
+                "keyTimes",
+                format!("0;{};{};1", start_growing, stop_growing),
+            );
+
+        let animated_colour_definition = Animate::new()
+            .set("attributeName", "fill")
+            .set("dur", format!("{}s", duration))
+            .set("repeatCount", "indefinite")
+            .set("values", "red;red;red;purple;purple;white")
+            .set(
+                "keyTimes",
+                format!(
+                    "0;{};{};{};{};1",
+                    start_growing, stop_growing, stop_colour_change, start_fading
+                ),
+            );
+
+        let animated_circle_definition = circle_definition
+            .add(animated_radius_definition)
+            .add(animated_colour_definition);
+
+        definitions = definitions.add(animated_circle_definition);
+    }
 
     let mut document = Document::new()
         .set("viewBox", (0, 0, image_width, image_height))
@@ -165,19 +199,14 @@ fn main() {
             Rectangle::new()
                 .set("width", image_width)
                 .set("height", image_height)
-                .set("fill", "cyan"),
+                .set("fill", "white"),
         );
-
-    document = document.add(
-        Use::new()
-            .set("href", "#circle")
-            .set("x", image_width / 2.0)
-            .set("y", image_height / 2.0),
-    );
 
     for ring in 0..=grid_size {
         if ring == 0 {
-            document = document.add(Use::new().set("href", "#hexagon"));
+            document = document
+                .add(Use::new().set("href", "#level0-circle"))
+                .add(Use::new().set("href", "#hexagon"));
         } else {
             for spoke in 0..=5 {
                 for offset in 0..ring {
@@ -185,6 +214,20 @@ fn main() {
                     let ring_radius = ring as f64 * hexagon_height;
                     let offset_angle = spoke_angle + 2.0 * hexagon_angle;
                     let offset_distance = offset as f64 * hexagon_height;
+                    document = document.add(
+                        Use::new()
+                            .set("href", format!("#level{}-circle", ring))
+                            .set(
+                                "x",
+                                ring_radius * (spoke_angle).cos()
+                                    + offset_distance * offset_angle.cos(),
+                            )
+                            .set(
+                                "y",
+                                ring_radius * (spoke_angle).sin()
+                                    + offset_distance * offset_angle.sin(),
+                            ),
+                    );
                     document = document.add(
                         Use::new()
                             .set("href", "#hexagon")
@@ -204,7 +247,10 @@ fn main() {
         }
     }
 
-    let svg_save_file_path = format!("{}/test1.svg", ANIMATION_FRAMES_DIRECTORY_NAME);
+    let svg_save_file_path = format!(
+        "{}/centered_hexagonal_numbers.svg",
+        ANIMATION_DIRECTORY_NAME
+    );
     match svg::save(&svg_save_file_path, &document) {
         Ok(_) => (),
         Err(error) => panic!(
